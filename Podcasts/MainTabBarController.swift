299f1e3bb9c0d48534211cdc20728ad3f848021d
iOS developer K <iOSdvlprK@gmail.com>
2023-01-03 03:04:34 +0900

    main tab bar controller setup

---
+++ Podcasts/MainTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  MainTabBarController.swift
+//  Podcasts
+//
+//  Created by joe on 2023/01/03.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    // MARK: - Setup Functions
+    
+    func setupViewControllers() {
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    // MARK: - Helper Functions
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+}
