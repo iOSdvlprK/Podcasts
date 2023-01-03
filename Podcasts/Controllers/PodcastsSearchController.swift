e3614886f9ad7ae626af854f20f1a06ac44420ad
iOS developer K <iOSdvlprK@gmail.com>
2023-01-03 19:26:04 +0900

    podcasts table view controller and search controller

---
+++ Podcasts/Controllers/PodcastsSearchController.swift
@@ -0,0 +1,59 @@
+//
+//  PodcastsSearchController.swift
+//  Podcasts
+//
+//  Created by joe on 2023/01/03.
+//
+
+import UIKit
+
+class PodcastsSearchController: UITableViewController, UISearchBarDelegate {
+    
+    let podcasts = [
+        Podcast(name: "Podcasts TableView Controller", artistName: "Joe K."),
+        Podcast(name: "UISearchController", artistName: "Some Author")
+    ]
+    
+    let cellId = "cellId"
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSearchBar()
+        setupTableView()
+    }
+    
+    // MARK: - Setup Work
+    
+    fileprivate func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        // later implement Alamofire to search iTunes API
+    }
+    
+    fileprivate func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    // MARK: - UITableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let podcast = self.podcasts[indexPath.row]
+        cell.textLabel?.text = "\(podcast.name)\n\(podcast.artistName)"
+        cell.textLabel?.numberOfLines = -1
+        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        return cell
+    }
+}
---
+++ Podcasts/Models/Podcast.swift
@@ -0,0 +1,13 @@
+//
+//  Podcast.swift
+//  Podcasts
+//
+//  Created by joe on 2023/01/03.
+//
+
+import Foundation
+
+struct Podcast {
+    let name: String
+    let artistName: String
+}
