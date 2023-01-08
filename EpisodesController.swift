b5a04dc79d62aaeadf23b6b746cf782f72cad375
iOS developer K <iOSdvlprK@gmail.com>
2023-01-09 01:34:49 +0900

    parse RSS XML feed with FeedKit

--- EpisodesController.swift
+++ EpisodesController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import FeedKit
 
 class EpisodesController: UITableViewController {
     
     var podcast: Podcast? {
         didSet {
             navigationItem.title = podcast?.trackName
+            
+            fetchEpisodes()
+        }
+    }
+    
+    fileprivate func fetchEpisodes() {
+        print("Looking for episodes at feed url:", podcast?.feedUrl ?? "")
+        
+        guard let feedUrl = podcast?.feedUrl else { return }
+        
+        let secureFeedUrl = feedUrl.contains("https") ? feedUrl : feedUrl.replacingOccurrences(of: "http", with: "https")
+        
+//        guard let url = URL(string: feedUrl) else { return }
+        guard let url = URL(string: secureFeedUrl) else { return }
+        let parser = FeedParser(URL: url)
+        parser.parseAsync { result in
+            print("Successfully parse feed:", result.self)
+            
+            // associative enumeration values
+            switch result {
+            case .success(let feed):
+                switch feed {
+                case let .rss(feed):
+                    var episodes = [Episode]()
+                    feed.items?.forEach({ feedItem in
+                        let episode = Episode(title: feedItem.title ?? "")
+                        episodes.append(episode)
+                    })
+                    self.episodes = episodes
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                default:
+                    print("Found a feed...")
+                }
+            case .failure(let error):
+                print("Failed to parse feed:", error)
+            }
         }
     }
     
