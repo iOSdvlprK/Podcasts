cd92f214bffe2fbdebd66f0be441ea7bee3a9ac9
iOS developer K <iOSdvlprK@gmail.com>
2023-01-12 05:10:52 +0900

    details design of podcast player

--- PlayerDetailsView.swift
+++ PlayerDetailsView.swift
@@ -11,6 +11,7 @@ class PlayerDetailsView: UIView {
     var episode: Episode! {
         didSet {
             titleLabel.text = episode.title
+            authorLabel.text = episode.author
             
             guard let url = URL(string: episode.imageUrl ?? "") else { return }
             episodeImageView.sd_setImage(with: url)
@@ -22,5 +23,12 @@ class PlayerDetailsView: UIView {
     }
     
     @IBOutlet weak var episodeImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 2
+        }
+    }
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    
 }
