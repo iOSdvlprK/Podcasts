412a6ad773ee111cffd2bfe37f105853be526f54
iOS developer K <iOSdvlprK@gmail.com>
2023-01-20 01:22:15 +0900

    Favorites collection view controller setup

---
+++ Podcasts/FavoritesController.swift
@@ -0,0 +1,49 @@
+//
+//  FavoritesController.swift
+//  Podcasts
+//
+//  Created by joe on 2023/01/20.
+//
+
+import UIKit
+
+class FavoritesController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCollectionView()
+    }
+    
+    fileprivate func setupCollectionView() {
+        collectionView.backgroundColor = .blue
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    // MARK: - UICollectionView Delegate / Spacing Methods
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .lightGray
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 3 * 16) / 2
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+}
