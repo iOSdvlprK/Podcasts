3c1a871543071ecbbc9093f6cade3bafa3d0a1fc
iOS developer K <iOSdvlprK@gmail.com>
2023-01-20 04:22:30 +0900

    Favorite podcast cell setup in code

--- Podcasts/FavoritesController.swift
+++ Podcasts/FavoritesController.swift
@@ -18,8 +18,8 @@ class FavoritesController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     fileprivate func setupCollectionView() {
-        collectionView.backgroundColor = .blue
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
+        collectionView.register(FavoritePodcastCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     // MARK: - UICollectionView Delegate / Spacing Methods
@@ -30,13 +30,12 @@ class FavoritesController: UICollectionViewController, UICollectionViewDelegateF
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .lightGray
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = (view.frame.width - 3 * 16) / 2
-        return CGSize(width: width, height: width)
+        return CGSize(width: width, height: width + 46)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
